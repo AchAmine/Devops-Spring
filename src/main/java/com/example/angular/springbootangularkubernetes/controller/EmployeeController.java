78e1b1df873e42c8cad5afc51c0bfe7e4175b8be
AchAmine <47825029+AchAmine@users.noreply.github.com>
2024-02-19 11:25:38 +0100

    Update EmployeeController.java with new logs

--- src/main/java/com/example/angular/springbootangularkubernetes/controller/EmployeeController.java
+++ src/main/java/com/example/angular/springbootangularkubernetes/controller/EmployeeController.java
@@ -82,5 +82,36 @@ public class EmployeeController {
         response.put("deleted", Boolean.TRUE);
         return response;
     }
+
+
+
+    @GetMapping("/")
+    public String HomePage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("Welcome home Page " + localDateTime);
+        return "Welcome to Home page";
+    }
+
+    @GetMapping("/logs")
+    public String LogsPage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.info("This Logs page " + localDateTime);
+        return "Welcome to logs page";
+    }
+
+    @GetMapping("/warn")
+    public String WarnPage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.warn("This warn page " + localDateTime);
+        return "Welcome to warn page";
+    }
+
+
+    @GetMapping("/er")
+    public String ErrorPage(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        log.error("This error page " + localDateTime);
+        return "Welcome to error page";
+    }
 }
 
