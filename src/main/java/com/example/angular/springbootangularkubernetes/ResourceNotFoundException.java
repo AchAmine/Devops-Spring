fc5a93ce47cb393fc89c35b7e8de8bd2b446cd87
AchAmine <47825029+AchAmine@users.noreply.github.com>
2024-02-20 18:41:56 +0100

    Update ResourceNotFoundException.java

--- src/main/java/com/example/angular/springbootangularkubernetes/ResourceNotFoundException.java
+++ src/main/java/com/example/angular/springbootangularkubernetes/ResourceNotFoundException.java
@@ -3,31 +3,10 @@ package com.example.angular.springbootangularkubernetes;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public interface CustomHttpException {
-    String getMessage();
-    HttpStatus getHttpStatus();
-}
-
-public class ResourceNotFoundException extends RuntimeException implements CustomHttpException {
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
         super(message);
-    }
-
-    @Override
-    public HttpStatus getHttpStatus() {
-        return HttpStatus.NOT_FOUND;
-    }
-}
-
-public class ForbiddenException extends RuntimeException implements CustomHttpException {
-
-    public ForbiddenException(String message) {
-        super(message);
-    }
-
-    @Override
-    public HttpStatus getHttpStatus() {
-        return HttpStatus.FORBIDDEN;
     }
 }
