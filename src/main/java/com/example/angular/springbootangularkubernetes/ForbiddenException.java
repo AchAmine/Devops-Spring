77811b7e0a8cdb788637f193df8d3b10acb8e483
AchAmine <47825029+AchAmine@users.noreply.github.com>
2024-02-20 18:49:38 +0100

    Update ForbiddenException.java

--- src/main/java/com/example/angular/springbootangularkubernetes/ForbiddenException.java
+++ src/main/java/com/example/angular/springbootangularkubernetes/ForbiddenException.java
@@ -3,10 +3,10 @@ package com.example.angular.springbootangularkubernetes;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
+    public ForbiddenException(String message) {
         super(message);
     }
 }
